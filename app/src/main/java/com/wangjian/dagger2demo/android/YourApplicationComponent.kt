5063ef5bc487fcbae617ad60b92fabecc375b7a3
wangjian <1637299051@qq.com>
2022-12-04 17:12:57 +0800

    modify dagger-android example

--- app/src/main/java/com/wangjian/dagger2demo/android/YourApplicationComponent.kt
+++ app/src/main/java/com/wangjian/dagger2demo/android/YourApplicationComponent.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import dagger.Component
 import dagger.android.AndroidInjectionModule
 
+//注意：针对安卓中使用，除了自己的module，还需要加入AndroidInjectionModule::class
 @Component(modules = [AndroidInjectionModule::class,YourActivityModule::class])
 interface YourApplicationComponent {
     fun inject(application: YourApplication)
